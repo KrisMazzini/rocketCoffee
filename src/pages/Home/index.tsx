be72f033acfaf797aa491152a8eef280e8eda396
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-31 10:01:41 -0300

    add desktop background

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,10 @@
+import { Blur } from "./styles"
+
 export function Home() {
     return (
-        <></>
+        <>
+            <Blur left="29px" reverseAnimation={true}></Blur>
+            <Blur left="calc(100% - 233px)" reverseAnimation={false}></Blur>
+        </>
     )
 }
