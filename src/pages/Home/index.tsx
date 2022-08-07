d5120f6807644682da8b4f173587e95f745d9733
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-08-07 18:32:02 -0300

    add main content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
-import { Container } from "./styles"
+import { Container, Main } from "./styles"
 import { Blur } from "../../components/Blur/styles"
 import { Menu } from "../../components/Menu"
+
+import RocketCoffee from "../../assets/rocket-coffee.png";
 
 export function Home() {
     return (
@@ -9,6 +11,11 @@ export function Home() {
                 <header>
                     <Menu />
                 </header>
+                <Main>
+                    <h1>Great Coffee</h1>
+                    <h2>&lt; Great Code / &gt;</h2>
+                    <img src={RocketCoffee} alt="" />
+                </Main>
             </Container>
             <Blur left="29px" reverseAnimation={true}></Blur>
             <Blur left="calc(100% - 233px)" reverseAnimation={false}></Blur>
