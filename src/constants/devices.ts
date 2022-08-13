d4deb752bdb9e8fb348f35826b3065dba7c0af8d
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-08-13 18:08:41 -0300

    restart project from scratch

--- src/constants/devices.ts
+++
@@ -1,7 +0,0 @@
-type Device = {
-    maxWidth: number;
-}
-
-export const smallDesktop:Device = {maxWidth: 1000}
-export const tablet:Device = {maxWidth: 850}
-export const mobile:Device = {maxWidth: 600}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,6 @@
-import { Container, Main } from "./styles"
-import { Blur } from "../../components/Blur/styles"
-import { Menu } from "../../components/Menu"
-
-import RocketCoffee from "../../assets/rocket-coffee.png";
-
 export function Home() {
     return (
         <>
-            <Container>
-                <header>
-                    <Menu />
-                </header>
-                <Main>
-                    <h1>Great Coffee</h1>
-                    <h2>&lt; Great Code / &gt;</h2>
-                    <img src={RocketCoffee} alt="" />
-                </Main>
-            </Container>
-            <Blur left="29px" reverseAnimation={true}></Blur>
-            <Blur left="calc(100% - 233px)" reverseAnimation={false}></Blur>
         </>
     )
 }
