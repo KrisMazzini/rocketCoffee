c780e5b2e8f8b6634625a09aaf3359ce18bcf96d
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-08-23 21:23:00 -0300

    create window context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home'
+import { WindowContext } from './contexts/WindowContext'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 )
+
+function App() {
+  const [windowSize, setWindowSize] = useState<string>(getWindowSize())
+
+  function getWindowSize() {
+    const { innerWidth: windowWidth } = window
+    
+    if (windowWidth < 600) {
+      return "mobile"
+    }
+
+    if (windowWidth < 900) {
+      return "tablet"
+    }
+
+    return "desktop"
+  }
+
+  function handleResize() {
+    const size = getWindowSize()
+    setWindowSize(size)
+  }
+
+  useEffect(() => {
+      window.addEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <WindowContext.Provider value={windowSize}>
+      <Home />
+    </WindowContext.Provider>
+  )
+}
